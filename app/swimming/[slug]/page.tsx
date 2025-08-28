249f75855669708bcace92cddd7368223b14f07c
Kailey Simons <kksimons@mit.edu>
2025-08-28 18:53:02 -0400

    mvp

--- app/swimming/[slug]/page.tsx
+++ app/swimming/[slug]/page.tsx
@@ -35,7 +35,7 @@ export function generateMetadata({ params }) {
       description,
       type: 'article',
       publishedTime,
-      url: `${baseUrl}/blog/${post.slug}`,
+      url: `${baseUrl}/swimming/${post.slug}`,
       images: [
         {
           url: ogImage,
@@ -74,7 +74,7 @@ export default function Blog({ params }) {
             image: post.metadata.image
               ? `${baseUrl}${post.metadata.image}`
               : `/og?title=${encodeURIComponent(post.metadata.title)}`,
-            url: `${baseUrl}/blog/${post.slug}`,
+            url: `${baseUrl}/swimming/${post.slug}`,
             author: {
               '@type': 'Person',
               name: 'My Portfolio',
